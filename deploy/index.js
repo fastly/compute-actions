c6425c995dad474ccbda7e41f3a1c31ec349217a
Kailan Blanks <kblanks@fastly.com>
2020-11-05 13:05:35 +0000

    Upload build artifact after deploy

--- deploy/index.js
+++ deploy/index.js
@@ -1,13 +1,32 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const artifact = require('@actions/artifact');
+const glob = require('@actions/glob');
+const path = require('path');
+
+const projectDirectory = core.getInput('project_directory');
 
 checkCLI().then(() => {
   return exec.exec('fastly', ['compute', 'deploy'], {
-    cwd: core.getInput('project_directory')
-  });
+    cwd: projectDirectory
+  }).then(uploadArtifact);
 }).catch((err) => {
   core.setFailed(err.message);
 });
+
+async function uploadArtifact() {
+  const globber = await glob.create(path.join(projectDirectory, 'pkg/*'));
+  const files = await globber.glob();
+
+  if (files.length < 1) {
+    throw "There is no archive in the pkg directory to upload.";
+  }
+
+  const artifactName = path.parse(files[0]).name;
+
+  const artifactClient = artifact.create();
+  const uploadResponse = await artifactClient.uploadArtifact(artifactName, files, '.', {});
+}
 
 async function checkCLI() {
   try {
