c82f5bd5858c019862d8d3faa86edefde20b9901
Integralist <mark.mcdx@gmail.com>
2023-02-13 14:06:32 +0000

    fix: remove --skip-verification (removed from CLI 6.x)

--- build/index.js
+++ build/index.js
@@ -4,11 +4,9 @@ const exec = require('@actions/exec');
 const checkCLI = require('../util/cli');
 
 const verbose = core.getBooleanInput('verbose');
-const skipVerification = core.getBooleanInput('skip_verification');
 
 checkCLI().then(() => {
   let params = ['compute', 'build', '--non-interactive'];
-  if (skipVerification) params.push('--skip-verification');
   if (verbose) params.push('--verbose');
 
   return exec.exec('fastly', params, {
